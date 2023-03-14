ec1d1457471a69659b7ce33bdab4ce5dcb211daa
phatcana116 <phatcana116@gmail.com>
2023-03-14 21:29:59 +0700

    Trang 35

--- ProjectBigSchool/Controllers/CoursesController.cs
+++ ProjectBigSchool/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using ProjectBigSchool.Models;
+﻿using Microsoft.AspNet.Identity;
+using ProjectBigSchool.Models;
 using ProjectBigSchool.Models.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,8 @@ namespace ProjectBigSchool.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: Courses
+        [Authorize]
+
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -24,6 +27,28 @@ namespace ProjectBigSchool.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
